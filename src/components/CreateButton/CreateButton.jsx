ff1e1d2f7ac24cfae3757ea9cf20561271b1fce0
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-05 10:35:49 +0600

    create chnage tender

--- src/components/CreateButton/CreateButton.jsx
+++ src/components/CreateButton/CreateButton.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import classes from './CreateButton.module.scss'
-import { Link } from 'react-router-dom'
-export default function CreateButton({children}) {
+import React from "react";
+import classes from "./CreateButton.module.scss";
+import { Link } from "react-router-dom";
+export default function CreateButton({ children }) {
   return (
-    <Link to={"tender/create/"} className={classes.CreateButton}>{children}</Link>
-  )
+    <Link to={"tender/create/"} className={classes.CreateButton}>
+      {children}
+    </Link>
+  );
 }
